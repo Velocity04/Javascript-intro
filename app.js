a57fab4260ec7d99c00fe1d0e4d583e2ec37df18
Princewill Onotu <princewill0401@gmail.com>
2022-06-22 17:38:23 +0100

    changed

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-document.write("<h3>Hello everyone, I'm Onotu Princewill</h3><p>I am 5'10</p><p>I am from Nigeria.</p>");
+alert ("Onotu Princewill height:5'10 I am from Nigeria.")
+// document.write ("<h3>Hello everyone, I'm Onotu Princewill</h3><p>I am 5'10</p><p>I am from Nigeria.</p>");
